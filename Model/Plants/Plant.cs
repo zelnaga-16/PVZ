353d4c527b587ccda3c145fc43d88cacb0520108
Ярослав Ровнянский <jarizleifr06@gmail.com>
2024-12-14 15:18:13 +0100

    Before Rethinking

--- Model/Plants/Plant.cs
+++ Model/Plants/Plant.cs
@@ -8,5 +8,7 @@ namespace Model.Plants;
 
 public abstract class Plant
 {
-
+    protected int Health {  get; set; }
+    protected int Damage { get; set; }
+    protected AttackType.Attack Attack { get; set; }
 }
---
+++ Model/Zombies/Zombie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Zombies;
+
+public abstract class Zombie
+{
+
+}
